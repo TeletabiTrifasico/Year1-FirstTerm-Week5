bae82d1161c0697b93262bd47bda803aa2802529
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:47:15 +0100

    Add files via upload

---
+++ assignment4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+            int counter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Entre a number (0=stop): ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                arr[i] = input;
+                if (input == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Enter a searchvalue: ");
+            int searchvalue = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0;i < arr.Length;i++) 
+            {
+                if (arr[i] == searchvalue)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Number of occurences of searchvalue ({searchvalue}) is: {counter}");
+        }
+    }
+}
