9161f7fcb6a4ca2f91939ce747adb83dca4ed983
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:44:57 +0100

    Add files via upload

---
+++ assignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Metrics;
+using System.Globalization;
+
+namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] arr = new int[20];
+            int smallest = 200;
+            int counter = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                arr[i] = random.Next(0, 150);
+                Console.WriteLine($"Element {i} = {arr[i]}");
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                    counter = 1;
+                }
+                else if (arr[i] == smallest)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"smallest number = {smallest}");
+            Console.WriteLine($"number of occurence = {counter}");
+        }
+    }
+}
