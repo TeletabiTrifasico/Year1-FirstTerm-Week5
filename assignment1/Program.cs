751cdf41739f47eb589c5ba65b7e24c6ba9eef13
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 17:44:28 +0100

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+
+            Random random = new Random();
+            int[] arr = new int[20];
+            double average = 0;
+            double difference = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                arr[i] = random.Next(0, 201);
+                Console.WriteLine($"Element {i} is {arr[i]}");
+                average += arr[i];
+            }
+
+            Console.WriteLine();
+            average /= 20;
+            Console.WriteLine($"The average is: {average:0.00}");
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                difference = Math.Abs(arr[i] - average);
+                Console.WriteLine($"Difference between average and element {i} is {difference:0.00}");
+            }
+        }
+    }
+}
